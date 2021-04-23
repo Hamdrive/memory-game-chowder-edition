036781f206b6769bc19b7ae0c8d3a454cbd2466d
Hamza <hamza.husein017@gmail.com>
2021-04-23 12:59:12 +0530

    commit

--- src/components/ScoreCounter.js
+++ src/components/ScoreCounter.js
@@ -1,6 +1,6 @@
 const ScoreCounter = ({currentScore, bestScore}) =>{
     return (
-		<div className="flex flex-col items-center text-2xl font-serif font-medium rounded-xl border-8 border-gray-800 p-8 bg-white w-96">
+		<div className="flex flex-col items-center text-2xl font-serif font-medium rounded-xl border-8 border-gray-800 p-8 bg-white ">
 			<div className="w-9/12 -mt-4 mb-2">
 				<p>Current Score: {currentScore}</p>
 			</div>
